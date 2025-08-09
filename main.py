2fdaade883cc8ba0b3ddf29ca7641f59b7a0f597
Abderahim <ita27rmp100@gmail.com>
2025-08-09 16:48:15 +0100

    project done

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ except :
 # vars 
 intro = "# py-cl-chabot -> "
 # Generate a request & chat with the bot
-def request_response(prompt, max_tokens=4096, temperature=0.7):
+def request_response(prompt,history, max_tokens=4096, temperature=0.7):
     client = Groq(
         api_key=os.getenv('groq_api'),
     )
@@ -24,14 +24,14 @@ def request_response(prompt, max_tokens=4096, temperature=0.7):
         messages=[
             {
                 "role": "user",
-                "content":prompt,
+                "content":f"Previous Chat : {history} \n Current prompt : {prompt}",
             }
         ],
         model="llama-3.3-70b-versatile",
         stream=False,
     )
     return chat_completion.choices[0].message.content
-def chatting(name) :
+def chatting(name,history="") :
     print(colored(f"{intro} bot : ","red") +" Hello, how can I assist you today? ")
     while True :
         you = input(colored(f"{intro} you : ","green"))
@@ -39,17 +39,17 @@ def chatting(name) :
             print(colored(f"{intro} bot : ","red") +" GoodBye hannouni.")
             break
         else :
-            bot = request_response(you)
+            bot = request_response(you,history)
             print(colored(f"{intro} bot : ","red") + bot)
-            # Use parameterized query for table name
             db.execute(f"INSERT INTO \"{name}\" (you, bot) VALUES (?, ?);", (you, bot))
+            history += f"User : {you} -> the answer of bot : {bot} \n"
 # options' functions
 def NewChat() :
     name = input(colored(f"{intro} Please, Enter the name of new chat : ",'blue'))
     try :
         db.execute("INSERT INTO chats(chatname) VALUES (?)", (name,))
         db.execute(f"CREATE TABLE \"{name}\" (you varchar(2048), bot varchar(2048))")
-        chatting(name)
+        chatting(name,"")
     except:
         print(colored("ERROR, This chat is already exist.","yellow"))
     
@@ -58,7 +58,9 @@ def OldChat() :
     try :
         db.execute(f"SELECT * FROM '{name}'")
         data = db.fetchall()
+        history = ""
         for msg in data :
+            history += f"User : {str(msg[0])} -> the answer of bot : {str(msg[1])} \n"
             print(
                 colored(f"{intro} you : ","green")+str(msg[0]+'\n'),
                 colored(f"{intro} bot : ","red")+str(msg[1]+'\n'),
@@ -89,7 +91,8 @@ def DeleteChat() :
 print(colored(f"{intro} Welcome to py-cl-chatbot.","blue"))
 while True :
     print(
-        colored(" Please choose one of the following options:", "blue"),
+        colored("-"*50,"blue"),
+        colored("\nPlease choose one of the following options:", "blue"),
         """
             -> Close the script : press 0
             -> Start a new chat: press 1
@@ -97,17 +100,20 @@ while True :
             -> Display the list of chats: press 3
             -> Delete a previous chat: press 4
         """)
-    select = int(input(colored(f"{intro} Please select the number that represents the service you want: ", "blue")))
-    if select == 0 :
-        print(colored(f"{intro} Thank you for using py-cl-chatbot, See you next time.", "blue"))
-        break
-    elif select == 1 :
-        NewChat()
-    elif select == 2 : 
-        OldChat()
-    elif select == 3 :
-        ListChats()
-    elif select == 4 :
-        DeleteChat()
-    else :
+    try :
+        select = int(input(colored(f"{intro} Please select the number that represents the service you want: ", "blue")))
+        if select == 0 :
+            print(colored(f"{intro} Thank you for using py-cl-chatbot, See you next time.", "blue"))
+            break
+        elif select == 1 :
+            NewChat()
+        elif select == 2 : 
+            OldChat()
+        elif select == 3 :
+            ListChats()
+        elif select == 4 :
+            DeleteChat()
+        else :
+            print(colored(f"{intro} Wrong input, please read the instructions carefully and try again","yellow"))
+    except :
         print(colored(f"{intro} Wrong input, please read the instructions carefully and try again","yellow"))
